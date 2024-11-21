bf7691c98781896398e7ec26d35a2f6b0251de33
Gadha Lekshmi P <gadhalekshmip@gmail.com>
2024-11-21 13:36:01 +0530

    data validation completed

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # datascienceproject
 
-## commands : create env : ''conda create -p venv python==3.100 -y''
+## commands : create env : ''conda create -p venv python==3.10 -y''
+
+## create template that will have entire folder structure
+
+### seting logger
+
+## configboc , ensure annotation ,read yaml a
+
+-> Data ingestion , constant , research- classes and fn
+
+
+### Workflows -- ML PIPELINE
+1. Data ingestion
+2. Data transformation
+3. Model trainer
+4. Model Evaluation
+
+## workflows
+
+1. Update config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update the entity
+5. Update the configuration manager in src config
+6. Update the components
+7. Update the pipeline 
+8. Update the main.py
--- main.py
+++ main.py
@@ -1,2 +1,23 @@
 from src.datascience import logger
-logger.info("Welcome to our custom logging in datascience")
+from src.datascience.pipeline.data_ingestion import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.initiate_data_ingestion()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\n x=======x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation stage"
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
